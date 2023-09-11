79afdc00076162a92099a800f02ccf4a214ad10a
Tanvir286 <tanvirdiu200@gmail.com>
2023-09-12 00:21:14 +0600

    Responsive

---
+++ js/script.js
@@ -0,0 +1,36 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily:{
+          open: ["Open Sans"],
+        },
+        fontSize:{
+          22: "22px",
+          64: "64px",
+          96:  "96px",
+          88: "88px",
+      },
+      colors:{
+        primary:"#FBD784",
+       },
+       spacing:{
+         462:"462px",
+         200:"200px",
+         286:'286px',
+         1320:"964px"
+       },
+       maxWidth:{
+        container:"1320px",
+        796: "796px",
+        405: "405px",
+        467: "467px",
+        559: "559px",
+        655: "519px",
+        587: "587px",
+        496:"496px",
+        771:"771px",
+        893:"893px",
+       },
+      },
+    },
+  };
